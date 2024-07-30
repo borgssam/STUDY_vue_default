741b6120dbeaa5744847d4070a14dbde1fc7a745
borgssam <borgssam@gmail.com>
2024-07-30 15:08:45 +0900

    배포1

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir:"docs",
 })
+
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true
+// })
